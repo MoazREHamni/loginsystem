60cf265713473382271527d16dd0fb90e473d6ef
MoazREHamni <142569794+MoazREHamni@users.noreply.github.com>
2024-05-11 13:18:34 +0500

    Update p-signup.php

--- p-signup.php
+++ p-signup.php
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-    <?php require 'partials/_nav.php' ?>
+    <?php require '_nav.php' ?>
     <?php
     if ($showAlert) {
         echo '
